b34250f00c91a364d6f1d57c5a9d23b8771626bc
hflag <532843488@qq.com>
2018-06-18 15:54:22 +0800

    '添加了aboutus.html页面，完成其视图和路由添加，并且设置了页面的网格系统'

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,8 @@ from django.views.generic.base import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'myapp/index.html')
+
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'myapp/aboutus.html')
